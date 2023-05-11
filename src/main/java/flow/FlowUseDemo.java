9c99dd7d6d106fe80e5627c1b9f01f89748118c2
meSalpiglossis <125427233+meSalpiglossis@users.noreply.github.com>
2023-05-12 01:42:06 +1000

    Fluent Interface demo

---
+++ src/main/java/flow/FlowUseDemo.java
@@ -0,0 +1,10 @@
+package flow;
+
+public class FlowUseDemo {
+    public static void main(String[] args) {
+
+        TimeMachine timeMachine = new TimeMachine();
+
+        timeMachine.setYear("1984 ").setMonth("05").setDay("12").setHyperModeOn().pushTheButton();
+    }
+}
